393c561c75ffce682f8618161e9619acfebcfb47
corekyle <core.kyle@student.ccm.edu>
2026-09-15 14:10:24 -0400

    Week 02 Lab 01 Problem 02 Complete

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,3 +1,36 @@
-public class CelsiusTable {
+public class CelsiusTable 
+{   // START OF class
     
+    public static void main(String[] args)
+    {   // START OF main
+        
+        // Declare variables
+        double fahrenheit;
+        double celsius;
+
+        // Create fahrenheit to celsius conversion table
+        for(int count = 0; count <=20; count++)
+        {   // START OF for
+            fahrenheit = count; // Increase fahrenheit by 1
+
+            celsius = celsius(fahrenheit);    // Fahrenheit to celsius calculation
+
+            System.out.printf("%,.1f F | %,.1f C\n", fahrenheit, celsius);
+        }   // END OF for
+
+
+    }   // END OF main
+
+    public static double celsius(double fahrenheit)
+{
+    // Declare variables
+    double c;
+    double f = fahrenheit;
+
+    // Calculation
+    c = 5.0 / 9.0 * (f - 32.0);
+
+    // Return value to main method
+    return c;
 }
+}   // END OF class
